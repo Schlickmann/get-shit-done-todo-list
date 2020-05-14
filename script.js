a1bcd4af9075c868ff37092b14af244cc72def11
Juliani Schlickmann Damasceno <nany.julychi@gmail.com>
2020-05-14 11:39:30 -0700

    Started js code

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-// App script
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+  // Prevent form from subimitting
+  event.preventDefault();
+
+  // Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+
+  todoDiv.appendChild(newTodo);
+
+  // Check mark BUTTON
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML = '<i class="fas fa-check"></i>';
+  completeButton.classList.add("complete-btn");
+
+  // Append to TodoDiv
+  todoDiv.appendChild(completeButton);
+
+  // Check delete BUTTON
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+  deleteButton.classList.add("delete-btn");
+
+  // Append to TodoDiv
+  todoDiv.appendChild(deleteButton);
+
+  // Append to TodoList
+  todoList.appendChild(todoDiv);
+}
